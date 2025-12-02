752f7e35dfc4bb6bf3436df772ce0ed0c279f9f7
babak <111069294+babakyousefian@users.noreply.github.com>
2025-12-02 03:46:27 -0500

    added the runner.sh
    added the runner.sh

---
+++ runner.sh
@@ -0,0 +1,9 @@
+echo "====== @Author by: babak yousefian ======"
+sleep 3
+echo "====== run is starting...!!! ======"
+sleep 4
+
+python3 main.py --n-iters 100 --tol 1e-6 -k 202 --lambda-reg 0.1
+
+echo "====== Done.! ======"
+sleep 3
